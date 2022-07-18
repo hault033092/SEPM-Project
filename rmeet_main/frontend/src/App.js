93dd71cd3b0ec1a8d1c0a8f404b1a5687f8c1ad8
hault033092 <letrunghau090298@gmail.com>
2022-07-18 09:50:18 +0700

    test with hello world

--- rmeet_main/frontend/src/App.js
+++ rmeet_main/frontend/src/App.js
@@ -1,8 +1,11 @@
-import logo from './logo.svg'
-import './App.css'
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
-  return <div className='App'></div>
+  return (
+    <div className='App'>
+      <h1>Hello world</h1>
+    </div>
+  )
 }
 
 export default App
