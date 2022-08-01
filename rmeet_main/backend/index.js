8187cad8db3a218acac827e000d490b58b406144
hault033092 <letrunghau090298@gmail.com>
2022-08-02 00:42:33 +0700

    Revert "Merge pull request #6 from hault033092/Backend-Tom"
    This reverts commit fd5b7750e7ecd366060ba6eee93400d6a0cbe62e, reversing
    changes made to f27791cd9b6b39f1d827ab33f264b27b64ffc713.

--- rmeet_main/backend/index.js
+++ rmeet_main/backend/index.js
@@ -2,31 +2,21 @@ const express = require('express')
 const PORT = 8080
 const app = express()
 const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
+// const bodyParser = require('body-parser')
 const cors = require('cors')
 require('dotenv/config')
 
 //Import route
 const authRoute = require('./routes/register')
 const getUsers = require('./routes/getUser')
-
-const deleteUser = require('./routes/deleteUser')
-const updateUser = require('./routes/updateUser')
-
 const postRoute = require('./routes/posts')
 
-
 //Middlewares
-// app.use(cors())
+app.use(cors())
 app.use(express.json())
 app.use('/api/user', authRoute)
 app.use('/api/user', getUsers)
-
-app.use('/api/user', deleteUser)
-app.use('/api/user', updateUser)
-
 app.use('/api/posts', postRoute)
-
 //
 
 //
