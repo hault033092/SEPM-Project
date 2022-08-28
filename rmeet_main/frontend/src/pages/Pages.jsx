15cd308801d1129c75f14148afeac6de79bf7d7b
Giang Nhật Khánh <nhatkhanhgiang2512@gmail.com>
2022-08-28 21:44:14 +0700

    added user profile page (responsive)

--- rmeet_main/frontend/src/pages/Pages.jsx
+++ rmeet_main/frontend/src/pages/Pages.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Routes, Route } from "react-router-dom"
 import Home from "./Home"
-import Timetable from "./Timetable"
 import Account from "./Account"
 import CourseReview from "./CourseReview"
 import CreatePost from "./CreatePost"
@@ -10,7 +9,6 @@ const Pages = () => {
   return (
         <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/timetable" element={<Timetable />} />
             <Route path="/account" element={<Account />} />
             <Route path="/review-course" element={<CourseReview />}></Route>
             <Route path="/board/create-post" element={<CreatePost />}></Route>
